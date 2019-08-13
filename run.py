04543167c932c3fbb3715d83f8f3f24846e3e3af
ChikaKanu <chika.kanu@procompletions.com>
2019-08-13 16:12:09 +0100

    object oriented draft

--- run.py
+++ run.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+class Item:
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
 
 
 @app.route("/")
 def hello_world():
-    items = ["Apple", "Pear", "Banana"]
+
+    # items = [
+    #     Item("Apple", 10),
+    #     Item("Pear", 5),
+    #     Item("Banana", 30)
+    # ]
+
+    items = [
+        {"name":"Apple", "amount": 10},
+        {"name": "Pear", "amount": 5},
+        {"name": "Banana", "amount": 10}
+    ]
     output = render_template("start.html", name="John Doe", items=items)
-    print (output)
+    print(output)
     return output
 
 
 @app.route("/test")
 def hello_name():
-    return render_template("test.html", name="John Doe")
+    args = request.args
+    name = args.get("name")
+    age = args.get("age")
+    return render_template("test.html", name=name, age=age)
